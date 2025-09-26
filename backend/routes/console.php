a38cdc61cd8fa30f95c4896ab0b0f71f0e63d284
Willson0 <logvinovnikita3@gmail.com>
2025-09-26 22:03:58 +0500

    0.1beta

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -9,3 +9,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command("schedule:check")->everyMinute();
+Schedule::command("subs:check")->hourly();
